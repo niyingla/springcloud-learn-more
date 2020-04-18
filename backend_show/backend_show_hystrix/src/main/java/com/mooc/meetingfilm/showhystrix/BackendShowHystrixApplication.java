a644422f1728d28a25ef5ec0d96c0c6b876b0a02
微笑只是问候 <1047984367@qq.com>
2020-04-19 01:45:11 +0800

    增加pom注释

--- backend_show/backend_show_hystrix/src/main/java/com/mooc/meetingfilm/showhystrix/BackendShowHystrixApplication.java
+++ backend_show/backend_show_hystrix/src/main/java/com/mooc/meetingfilm/showhystrix/BackendShowHystrixApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Hystrix 主要作用延迟和容错（熔断）
  * 目标 避免级联故障，提高系统弹性 （c挂了 a请求c一直在等待 a也不行了）
+ *
+ * 1 开始执行Hystrix （同步执行|异步执行两种）
+ * 3 判断缓存是否可用（是否开启缓存，有数据）
+ * 4 是否开启熔断 否 快速失败
+ * 5 信号量是否到达上限 是 快速失败
+ * 6 执行业务逻辑 超时 快速失败
+ * 
  */
 @SpringBootApplication
 public class BackendShowHystrixApplication {
