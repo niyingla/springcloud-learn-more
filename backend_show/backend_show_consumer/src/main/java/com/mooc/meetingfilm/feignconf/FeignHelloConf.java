27d2dc66b8d4c34ef0d7a66c3786ed33563917b4
1047984367@qq.com <as1314520>
2020-04-20 11:19:25 +0800

    初始化项目

--- backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/feignconf/FeignHelloConf.java
+++ backend_show/backend_show_consumer/src/main/java/com/mooc/meetingfilm/feignconf/FeignHelloConf.java
@@ -5,19 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 配置类不要放在Application目录下
+ * 配置类不要放在Application目录下 这类不需要@Configuration 注解。
+ * 如果加了这个你还需要在使用@ComponentScan 扫描的时候排除掉，否则就变成全局配置
  * @author : jiangzh
  * @program : com.mooc.meetingfilm.feignconf
  * @description :
  **/
-@Configuration
+@Configuration//配置需要当前注解
 public class FeignHelloConf {
 
     /**
      * 指定feign的Contract
      * @return
      */
-    @Bean
+    @Bean //配置需要当前注解
     public Contract contract(){
         return new feign.Contract.Default();
     }
