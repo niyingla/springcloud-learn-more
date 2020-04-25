ad2923fd5619c1b0276e58543f716019dfec8cad
微笑只是问候 <1047984367@qq.com>
2020-04-25 23:06:16 +0800

    增加代码注释

--- backend_show/backend_show_testng/src/main/java/com/mooc/meetingfilm/testng/BackendShowTestngApplication.java
+++ backend_show/backend_show_testng/src/main/java/com/mooc/meetingfilm/testng/BackendShowTestngApplication.java
@@ -3,6 +3,13 @@ package com.mooc.meetingfilm.testng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * docker 操作必须是root或者同等级权限
+ * docker 创建docker文件
+ * docker build -t 火华一号:1.0 .
+ *  创建名字为火华一号 版本为1.0的docker文件
+ *  docker run -itd 火华一号:1.0 后台运行这个镜像
+ */
 @SpringBootApplication
 public class BackendShowTestngApplication {
 
